71d18bdd6d059d34031ea0cc4390913bfd5473e2
vitorvieira210501-crypto <vitorvieira210501@gmail.com>
2025-10-06 18:05:31 -0300

    Update while05.js

--- AULA05/while05.js
+++ AULA05/while05.js
@@ -1,4 +1,4 @@
-// faça um progama para ler o valor e para mostrar a tabela de multiplicacao deste valor até 10 
+// faça um programa para ler o valor e para mostrar a tabela de multiplicacao deste valor até 10 
 let contador = 1;
 let valor = Number(prompt("QUAL VALOR VOCE QUER MULTIPLICAR"))
 
